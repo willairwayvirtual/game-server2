376462f4f858aa909afc70bfefb8e2043b1bd962
Lorcan <Lorcan@WAVSOFT>
2021-01-29 11:51:00 +0000

    Add project files.

---
+++ game server2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace game_server2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = ("game server");
+            Console.ReadKey();
+        }
+    }
+}
