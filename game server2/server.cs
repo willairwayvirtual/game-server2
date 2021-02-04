e17602a33c8fd6545e7724e2b603acc86c23a3af
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2021-02-04 12:38:04 +0000

    fqegwelpgsvoktpwgrefdik421e4er

--- game server2/server.cs
+++ game server2/server.cs
@@ -9,24 +9,25 @@ namespace game_server2
     class server
     {
 
-        public static int maxplayers { get; private set }
-        public static int por{ get; private set }
+        public static int maxplayers { get; private set; }
+        public static int port { get; private set; }
         public static TcpListener tcpListener;
         public static void Start(int _maxPlayers, int _port)
         {
-          maxplayers,= _maxPlayers;
-            _port = _port;
+            maxplayers = _maxPlayers;
+            port = _port;
 
             Console.WriteLine("Starting server...");
 
-             tcpListener = new TcpListener(IPAddress.Any, port);
-            Console.WriteLine($"Server started on port {Port}.);
-
-            private  static void TCPConnectCallback(IAsyncResult _result)
-            {
-                TcpClient _client = tcpListener.EndAcceptTcpClient(_result);
-                tcpListener.BeginAcceptTcpClient(TCPConnectCallback, null);
-                Console.WriteLine($"Incoming connection from {_client.Client.RemoteEndPoint}...");
-
-            }
+            tcpListener = new TcpListener(IPAddress.Any, port);
+            Console.WriteLine($"Server started on port {port}.");
         }
+        private static void TCPConnectCallback(IAsyncResult _result)
+        {
+            TcpClient _client = tcpListener.EndAcceptTcpClient(_result);
+            tcpListener.BeginAcceptTcpClient(TCPConnectCallback, null);
+            Console.WriteLine($"Incoming connection from {_client.Client.RemoteEndPoint}...");
+
+        }
+    }
+}
